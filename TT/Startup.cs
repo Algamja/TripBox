676de4f58875d4e1208ce1876bdb41e222ec629f
PROJECT <rlawkaud11@naver.com>
2020-04-11 20:31:08 +0900

    menu랑 사이드바

--- TT/Startup.cs
+++ TT/Startup.cs
@@ -56,7 +56,7 @@ namespace TT
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Main}/{action=Index}/{id?}");
             });
         }
     }
